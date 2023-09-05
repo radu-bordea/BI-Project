fff648e31c94974b564266217195f088eb911313
Bordea Radu <radu.bordea@ha.ax>
2023-09-05 13:18:50 +0200

    create keepr schema and post data

--- server/app.js
+++ server/app.js
@@ -6,8 +6,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// locations middleware
+app.get("/locations", mongoose.getLocations);
 app.post("/locations", mongoose.createLocation);
 
-app.get("/locations", mongoose.getLocations);
 
+// keepers middleware
+app.get("/keepers", mongoose.getKeepers)
+app.post("/keepers", mongoose.createKeeper)
+
+
+// listening the port
 app.listen(5000);
+
--- server/mongoose.js
+++ server/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const Location = require("./models/location");
+const Keeper = require("./models/keeper");
 
+// connect to mongo atlas bi database
 mongoose
   .connect(
     "mongodb+srv://radu:brn1989@cluster0.37mmycn.mongodb.net/bi-project?retryWrites=true&w=majority"
@@ -13,6 +15,16 @@ mongoose
     console.log("Connection failed!");
   });
 
+/******  MIDDLEWARE ******/
+
+/* === LOCATIONS === */
+// get locations from mongo atlas
+const getLocations = async (req, res, next) => {
+  const locations = await Location.find().exec();
+  res.json(locations);
+};
+
+// post location to mongo atlas
 const createLocation = async (req, res, next) => {
   const createdLocation = new Location({
     _id: req.body._id,
@@ -26,10 +38,31 @@ const createLocation = async (req, res, next) => {
   res.json(result);
 };
 
-const getLocations = async (req, res, next) => {
-  const locations = await Location.find().exec();
-  res.json(locations);
+/* === KEEPERS === */
+// get keepers from mongo atlas
+const getKeepers = async (req, res, next) => {
+  const keepers = await Keeper.find().exec();
+  res.json(keepers);
 };
 
-exports.createLocation = createLocation;
+// post keeper to mongo atlas
+const createKeeper = async (req, res, next) => {
+  const createdKeeper = new Keeper({
+    _id: req.body._id,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+  });
+  console.log(createdKeeper);
+  const result = await createdKeeper.save();
+  res.json(result);
+};
+
+/* === EXPORTS === */
+//locations
 exports.getLocations = getLocations;
+exports.createLocation = createLocation;
+// keepers
+exports.getKeepers = getKeepers;
+exports.createKeeper = createKeeper;
