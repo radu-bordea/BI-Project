4a2e0ca3d9591ff5c33391e7ed33ac01761a9d01
Bordea Radu <radu.bordea@ha.ax>
2023-09-10 13:33:34 +0200

    create data schema

---
+++ server/models/data.js
@@ -0,0 +1,12 @@
+const { Decimal128 } = require("mongodb");
+const mongoose = require("mongoose");
+
+const dataSchema = mongoose.Schema({
+  _id: { type: String, required: true },
+  deviceId: { type: String, required: true },
+  value: { type: Decimal128, required: true },
+  timeStamp: { type: Date, required: true },
+  registerTimeStamp: { type: Date, required: true },
+});
+
+module.exports = mongoose.model("Data", dataSchema);
