2b3b36c2312f3dd05f645960339a5d80db52fdee
Bordea Radu <radu.bordea@ha.ax>
2023-09-18 22:07:57 +0200

    create temperature and humidity graphics and retrieve devices

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -11,6 +11,8 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+import "./Content.css";
+import Devices from "./models/Devices";
 
 ChartJS.register(
   CategoryScale,
@@ -42,20 +44,21 @@ const options = {
 };
 
 function Content() {
-  const [checkData, setCheckData] = useState(false)
-  const [checkLocation, setCheckLocation] = useState(false)
-  const [checkKeeper, setCheckKeeper] = useState(false)
-  const [checkDevice, setCheckDevice] = useState(false)
-  const [checkType, setCheckType] = useState(false)
+  const [checkData, setCheckData] = useState(false);
+  const [checkLocation, setCheckLocation] = useState(false);
+  const [checkKeeper, setCheckKeeper] = useState(false);
+  const [checkDevice, setCheckDevice] = useState(false);
+  const [checkType, setCheckType] = useState(false);
 
   const [measurements, setMeasurements] = useState([]);
-
-
 
   const fetchData = async () => {
     try {
       const response = await axios.get("http://localhost:5000/data");
       setMeasurements(response.data); // Update to set the entire response.data array
+      console.log("====================================");
+      console.log(response.data);
+      console.log("====================================");
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -72,19 +75,39 @@ function Content() {
     fetchData();
   }, []);
 
-  const labels = measurements.map((measure) =>
-    formatTimestamp(measure.timeStamp)
-  );
-  const dataset = {
+  const labels = [
+    ...new Set(
+      measurements.map((measure) => formatTimestamp(measure.timeStamp))
+    ),
+  ];
+
+  const datasetTemp = {
     label: "Temperature",
-    data: measurements.map((measure) => measure.value.$numberDecimal),
+    data: measurements
+      .filter((measure) => measure.deviceId === "1")
+      .map((measure) => measure.value.$numberDecimal),
+    borderColor: "red", // Set line color to red
+    backgroundColor: "red", // Set point color to red
+  };
+  // const dataset2 = {
+  //   label: "Humidity",
+  //   data: measurements.map((measure) => measure.value.$numberDecimal),
+  // };
+
+  const datasetHum = {
+    label: "Humidity",
+    data: measurements
+      .filter((measure) => measure.deviceId === "2")
+      .map((measure) => measure.value.$numberDecimal),
+      borderColor: "blue", // Set line color to blue
+  backgroundColor: "blue", // Set point color to blue
   };
 
   const data = {
     labels: labels,
-    datasets: [dataset],
+    datasets: [datasetTemp, datasetHum],
+    // datasets: [dataset, dataset2],
   };
-
 
   const handleButtonClick = (buttonName) => {
     setCheckData(buttonName === "data");
@@ -92,24 +115,44 @@ function Content() {
     setCheckKeeper(buttonName === "keeper");
     setCheckDevice(buttonName === "device");
     setCheckType(buttonName === "type");
-  }
+  };
 
   return (
     <div className="content">
       <div className="content-header">
-        <button className="model model-data" onClick={() => handleButtonClick("data")}>DATA</button>
-        <button className="model" onClick={() => handleButtonClick("location")}>Locations</button>
-        <button className="model" onClick={() => handleButtonClick("keeper")}>Keepers</button>
-        <button className="model" onClick={() => handleButtonClick("device")}>Devices</button>
-        <button className="model" onClick={() => handleButtonClick("type")}>Types</button>
+        <button
+          className="model model-data"
+          onClick={() => handleButtonClick("data")}
+        >
+          DATA
+        </button>
+        <button className="model" onClick={() => handleButtonClick("location")}>
+          Locations
+        </button>
+        <button className="model" onClick={() => handleButtonClick("keeper")}>
+          Keepers
+        </button>
+        <button className="model" onClick={() => handleButtonClick("device")}>
+          Devices
+        </button>
+        <button className="model" onClick={() => handleButtonClick("type")}>
+          Types
+        </button>
       </div>
       <div className="content-graph">
         {checkData && measurements && measurements.length > 0 && (
-          <Line data={data} options={options} />
+          <div>
+            <Devices />
+            <Line data={data} options={options} />
+          </div>
         )}
         {checkLocation && <h1>Locations</h1>}
         {checkKeeper && <h1>Keepers</h1>}
-        {checkDevice && <h1>Devices</h1>}
+        {checkDevice && (
+          <h1>
+            <Devices />
+          </h1>
+        )}
         {checkType && <h1>Types</h1>}
       </div>
     </div>
