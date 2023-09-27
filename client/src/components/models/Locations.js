c2612883ef020f1ea4aa9c80f077414ae2891ed8
Bordea Radu <radu.bordea@ha.ax>
2023-09-27 23:20:06 +0200

    s

--- client/src/components/models/Locations.js
+++ client/src/components/models/Locations.js
@@ -1,35 +1,34 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./models.css";
 
-const Locations = () => {
+function Locations() {
   const [locations, setLocations] = useState([]);
 
-  useEffect(() => {
-    const fetchLocations = async () => {
-      try {
-        const response = await axios.get("http://localhost:5000/locations");
-        setLocations(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+  const fetchLocations = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/locations");
+      setLocations(response.data); // Update to set the entire response.data array
+      console.log(locations);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
+  useEffect(() => {
     fetchLocations();
-  }, [locations]); // Include locations in the dependency array
+  }, []);
 
   return (
-    <div className="lev1">
-      <div className="lev2">
+    <>
+      <hr />
+      <div>
         {locations.map((location, index) => (
-          <button className="btn" key={index}>
-            {location.name}
-          </button>
+          <button key={index}>{location.name}</button>
         ))}
       </div>
-    </div>
+    </>
   );
-};
+}
 
 export default Locations;
