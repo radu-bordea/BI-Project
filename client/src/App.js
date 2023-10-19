e8b041348cf72e78086910b652cc78e8f5d4c23f
Bordea Radu <radu.bordea@ha.ax>
2023-10-20 00:27:48 +0200

    styling map

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 // App.js
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Components/NavbarComponent';
-import Home from './Components/Home';
-import About from './Components/About';
-import Data from './Components/Data';
-import Map from './Components/Map';
-import './App.css';
-import Footer from './Components/Footer';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/NavbarComponent";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Data from "./Components/Data";
+import Map from "./Components/Map";
+import "./App.css";
+import Footer from "./Components/Footer";
 
 const App = () => {
   const [expanded, setExpanded] = useState(false);
@@ -18,24 +18,24 @@ const App = () => {
   };
 
   return (
-    <Router>
-      <div className='container'>
-        <Navbar
-          handleNavClick={handleNavClick}
-          expanded={expanded}
-          setExpanded={setExpanded}
-        />
-
+    <Router className="container-fluid">
+      <Navbar
+        handleNavClick={handleNavClick}
+        expanded={expanded}
+        setExpanded={setExpanded}
+        className="navbar navbar-expand-lg navbar-light bg-light fixed-top"
+      />
+      <div className="switch">
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/data" component={Data} />
           <Route path="/maps" component={Map} />
         </Switch>
+        <Footer className="fixed-bottom"/>
       </div>
-        <Footer className="footer"/>
     </Router>
   );
-}
+};
 
 export default App;
--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,25 +1,13 @@
-import { AiFillStar } from "react-icons/ai";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer className="footer fixed-bottom bg-light">
+    <footer className="bg-dark text-light text-center py-2 fixed-bottom" style={{height:"7vh"}}>
       <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <p className="mb-2 mb-md-0">Email: owner.right@gmail.com</p>
-            <p className="mb-2 mb-md-0">Phone: +358 909 676</p>
-          </div>
-          <div className="col-md-4">
-            <p className="mb-2 mb-md-0">Address: Life Street, City of the Moon, Univers, 234 12</p>
-          </div>
-          <div className="col-md-4">
-            <p className="mb-2 mb-md-0">Trust: <AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /></p>
-          </div>
-        </div>
+        <p>&copy; 2023 Your Company</p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
